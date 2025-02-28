8c8c8d86e09c1aa0ab05075f36b565c762567966
Ashish Sharma <ashish.sharma6@dotsquares.com>
2025-02-28 15:52:55 +0530

    changed in Diff.java

--- Diff.java
+++ Diff.java
@@ -1,3 +1,9 @@
 public class Diff {
-    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int diff = a - b;
+        System.out.println("Difference of a and b is: " + diff);
+        System.out.println("Hello World");
+    }
 }
