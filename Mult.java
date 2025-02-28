37c8f404dc7176ba60708cae1db77a6035cf575c
Ashish Sharma <ashish.sharma6@dotsquares.com>
2025-02-28 15:38:19 +0530

    added multiplication functionalities

---
+++ Mult.java
@@ -0,0 +1,9 @@
+public class Mult {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int mult = a * b;
+        System.out.println("Multiplication of a and b is: " + mult);
+        System.out.println("Hello World");
+    }
+}
